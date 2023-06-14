0786b1f9f338e0c7e3a1a349958db99636b54e5d
Spasoje Djurovic <spasoje.djurovic90@gmail.com>
2023-06-15 00:26:48 +0200

    remove localhost from weather fetch

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageError.innerHTML = '';
     messageOne.innerHTML = '';
     messageTwo.innerHTML = '';
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>{
+    fetch(`/weather?address=${location}`).then((response) =>{
     response.json().then((data) => {
         if(data.error) {
             messageError.innerHTML = data.error;
